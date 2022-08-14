e422db3fe80d4991fd479c45dc7ea1325adcd8df
vanbien2402 <vanbien2402@gmail.com>
2022-08-14 13:36:33 +0700

    demo REST api CRUD with postgres and docker

---
+++ internal/models/users.go
@@ -0,0 +1,10 @@
+package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	UserName string `json:"user_name"`
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
